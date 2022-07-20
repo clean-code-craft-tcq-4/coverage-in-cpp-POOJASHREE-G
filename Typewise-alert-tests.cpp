3e9d9786ea28ade911f90b166319b2899728682d
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-07-20 18:03:54 +0530

    Update test-alerts.cpp

---
+++ Typewise-alert-tests.cpp
@@ -0,0 +1,24 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "Typewise-alert.hpp"
+
+PASSIVE_COOLING passive_cooling;
+HI_ACTIVE_COOLING hiactive_cooling;
+MED_ACTIVE_COOLING medactive_cooling;
+
+
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+
+}
+
+TEST_CASE( "Classify temperature breach for Passive cooling") {
+
+  REQUIRE(classifyTemperatureBreach(passive_cooling, -1) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(passive_cooling, 36) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(passive_cooling, 35) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(passive_cooling, 0) == TOO_LOW);
+
+
+}
---
+++ Typewise-alert.hpp
@@ -0,0 +1,83 @@
+#ifndef TYPEWISE_ALERT_HPP
+#define TYPEWISE_ALERT_HPP
+
+
+class PASSIVE_COOLING
+{
+  public:
+  const int Lower_Limit = 0;
+  const int Upper_Limit = 35;
+};
+
+class HI_ACTIVE_COOLING
+{
+  public:
+  const int Lower_Limit = 0;
+  const int Upper_Limit = 45;
+};
+
+class MED_ACTIVE_COOLING
+{
+  public:
+  const int Lower_Limit = 0;
+  const int Upper_Limit = 40;
+};
+
+
+typedef enum {
+  NORMAL,
+  TOO_LOW,
+  TOO_HIGH
+} BreachType;
+
+static const char *BreachType_str[] = { "NORMAL", "TOO_LOW", "TOO_HIGH"};
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+
+typedef enum {
+  TO_CONTROLLER,
+  TO_EMAIL
+} AlertTarget;
+
+typedef struct {
+  CoolingType coolingType;
+  char brand[48];
+} BatteryCharacter;
+
+
+template <class CoolingType>
+struct BatteryCharacter {
+  CoolingType coolingType;
+  char brand[48];
+};
+
+template <class CoolingType>
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
+
+  return inferBreach(temperatureInC, coolingType.lowerLimit, coolingType.upperLimit);
+
+}
+
+template <typename CoolingType>
+bool checkAndAlert(
+  AlertTarget alertTarget, BatteryCharacter<CoolingType> batteryChar, double temperatureInC) {
+
+  BreachType breachType = classifyTemperatureBreach( batteryChar.coolingType, temperatureInC );
+
+  switch(alertTarget) {
+    case TO_CONTROLLER:
+      sendToController(breachType);
+      break;
+    case TO_EMAIL:
+      sendToEmail(breachType);
+      break;
+  }
+  return true;
+}
+
+void sendToController(BreachType breachType);
+void sendToEmail(BreachType breachType);
+
+const char* recepient = "a.b@c.com";
+
+#endif
